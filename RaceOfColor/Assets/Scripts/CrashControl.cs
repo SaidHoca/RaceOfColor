52ad113116471a2046964ca25b3f59ff28ede23f
SaidHoca <saidburhan@gmail.com>
2019-04-24 23:45:02 +0300

    Ön dizayn 2
    İkinci gün ancak birinci ciddi günün sonu.. 24 nisan.. iki adet script dosyası eklendi biri gamemanager.. diğeri ise infinity roadu ve nesneleri geri toplayacak olan crash scripti.. ekstra iyileştirmelere ihtiyaç duyulabilir...

---
+++ RaceOfColor/Assets/Scripts/CrashControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashControl : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        other.transform.position = new Vector3(0, 0, other.transform.position.z + 90);
+
+
+    }
+}
---
+++ RaceOfColor/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject Kirmizi,zemin, zemin1, zemin2 , zemin3;
+    public int hiz = -5;
+
+    void Start()
+    {
+        Kirmizi.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, hiz);
+        zemin.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, hiz);
+        zemin1.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, hiz);
+        zemin2.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, hiz);
+        zemin3.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, hiz);
+    }
+
+
+    void Update()
+    {
+   
+    }
+}
